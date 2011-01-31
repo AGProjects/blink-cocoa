0f035d939ae70cac150a1fdb3b91936852b1c3c8
Saul Ibarra <saul@ag-projects.com>
2011-01-31 08:48:59 +0000

    Removed unneeded SIPSimpleSettings instantiations

--- SessionRinger.py
+++ SessionRinger.py
@@ -158,7 +158,6 @@ class Ringer(object):
 
         if inbound_ringtone and not settings.audio.silent:
             # Workaround not to use same device from two bridges. -Saul
-            settings = SIPSimpleSettings()
             if settings.audio.alert_device is not None and app.alert_audio_mixer.real_output_device == app.voice_audio_mixer.real_output_device:
                 new_tone = WavePlayer(app.voice_audio_mixer, inbound_ringtone.path.normalized, volume=inbound_ringtone.volume, loop_count=0, pause_time=6)
                 app.voice_audio_bridge.add(new_tone)
@@ -178,7 +177,6 @@ class Ringer(object):
 
         if inbound_ringtone and not settings.audio.silent:
             # Workaround not to use same device from two bridges. -Saul
-            settings = SIPSimpleSettings()
             if settings.audio.alert_device is not None and app.alert_audio_mixer.real_output_device == app.voice_audio_mixer.real_output_device:
                 new_tone = WavePlayer(app.voice_audio_mixer, ResourcePath('ring_tone.wav').normalized, loop_count=0, pause_time=6, volume=inbound_ringtone.volume)
                 app.voice_audio_bridge.add(new_tone)
