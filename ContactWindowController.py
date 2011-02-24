8fc5627ee9c1075f795d66d086af3e76f5c8630a
Adrian Georgescu <ag@ag-projects.com>
2011-02-24 13:18:51 +0000

    Fixed indentation level

--- ContactWindowController.py
+++ ContactWindowController.py
@@ -1371,10 +1371,11 @@ class ContactWindowController(NSWindowController):
 
     def handle_incoming_proposal(self, session, streams):
         stream_type_list = list(set(stream.type for stream in streams))
+        
         if stream_type_list == ['chat'] and 'audio' in (s.type for s in session.streams):
-                BlinkLogger().log_info(u"Automatically accepting chat for established audio session from %s" % session.remote_identity)
-                self.acceptIncomingProposal(session, streams)
-                return
+            BlinkLogger().log_info(u"Automatically accepting chat for established audio session from %s" % session.remote_identity)
+            self.acceptIncomingProposal(session, streams)
+            return
         elif session.account is BonjourAccount():
             if stream_type_list == ['chat']:
                 BlinkLogger().log_info(u"Automatically accepting Bonjour chat session from %s" % session.remote_identity)
