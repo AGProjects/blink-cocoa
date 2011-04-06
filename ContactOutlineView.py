da9c1496a3fb91e7d6a6d0268798720790d0607e
Dan Pascu <dan@ag-projects.com>
2011-04-06 14:35:46 +0000

    Fixed context menu for no contact click

--- ContactOutlineView.py
+++ ContactOutlineView.py
@@ -7,14 +7,12 @@ from AppKit import *
 class ContactOutlineView(NSOutlineView):
     def menuForEvent_(self, event):
         self.window().makeFirstResponder_(self)
-        
         point = self.convertPoint_fromView_(event.locationInWindow(), None)
         row = self.rowAtPoint_(point)
-
+        if row < 0:
+            return None
         self.selectRowIndexes_byExtendingSelection_(NSIndexSet.indexSetWithIndex_(row), False)
-
         return self.menu()
-        
 
     def keyDown_(self, event):
         if event.characters() == "\r":
