5037a6af6172bf97234e1431f01211eb7b99a853
Adrian Georgescu <ag@ag-projects.com>
2011-06-03 10:40:31 +0000

    Fixed date selection range

--- HistoryViewer.py
+++ HistoryViewer.py
@@ -77,8 +77,8 @@ class HistoryViewer(NSWindowController):
     daily_order_fields = {'date': 'DESC', 'local_uri': 'ASC', 'remote_uri': 'ASC'}
     media_type_array = {0: None, 1: 'audio', 2: 'chat', 3: 'sms', 4: 'file-transfer', 5: 'audio-recording', 6: 'video-recording', 7: 'voicemail', 8: 'missed-call'}
     period_array = {0: None, 
-                    1: datetime.datetime.now()-datetime.timedelta(days=2), 
-                    2: datetime.datetime.now()-datetime.timedelta(days=7), 
+                    1: datetime.datetime.now()-datetime.timedelta(days=1),
+                    2: datetime.datetime.now()-datetime.timedelta(days=7),
                     3: datetime.datetime.now()-datetime.timedelta(days=31),
                     4: datetime.datetime.now()-datetime.timedelta(days=31),
                     5: datetime.datetime.now()-datetime.timedelta(days=90),
