9958045956592682cafdff311ce2d300106fe9e0
Adrian Georgescu <ag@ag-projects.com>
2011-09-10 09:43:52 +0000

    Always show panel with reason for registration error

--- ContactWindowController.py
+++ ContactWindowController.py
@@ -513,12 +513,13 @@ class ContactWindowController(NSWindowController):
             return
         self.accounts[position].registration_state = 'failed'
         self.refreshAccountList()
-        if isinstance(notification.sender, Account) and notification.data.error == 'Authentication failed':
+        if isinstance(notification.sender, Account):
             if not self.authFailPopupShown:
                 self.authFailPopupShown = True
-                NSRunAlertPanel(u"SIP Registration Error",
-                    u"The account %s could not be registered because of an authentication error. Either your account credentials are incorrectly entered or the SIP service is not available at this moment. \n\nFor help on this matter please contact your SIP service provider." % notification.sender.id,
-                    u"OK", None, None)
+                if notification.data.error == 'Authentication failed':
+                    NSRunAlertPanel(u"SIP Registration Error", u"The account %s could not be registered because of an authentication error. Please check if your account credentials are correctly entered. \n\nFor help on this matter you should contact your SIP service provider." % notification.sender.id, u"OK", None, None)
+                else:
+                    NSRunAlertPanel(u"SIP Registration Error", u"The account %s could not be registered at this time: %s" % (notification.sender.id, notification.data.error),  u"OK", None, None)
                 self.authFailPopupShown = False
 
     def _NH_SIPAccountRegistrationDidEnd(self, notification):
