6937d852ed08621595a8b1b626d58a0fea99a783
Adrian Georgescu <ag@ag-projects.com>
2012-02-13 09:14:48 +0000

    Disable TLS verify peer per account by default

--- configuration/account.py
+++ configuration/account.py
@@ -86,7 +86,7 @@ class SoundsSettings(SettingsGroup):
 
 class TLSSettingsExtension(TLSSettings):
     certificate = Setting(type=AccountTLSCertificate, default=AccountTLSCertificate(AccountTLSCertificate.DefaultTLSCertificate('default.crt')))
-    verify_server = Setting(type=bool, default=True)
+    verify_server = Setting(type=bool, default=False)
 
 
 class XCAPSettingsExtension(XCAPSettings):
