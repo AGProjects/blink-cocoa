d38187eed54db4e4cf448726d8cfc29b8fe68c59
Adrian Georgescu <ag@ag-projects.com>
2012-04-15 08:07:32 +0000

    Improved handling of ringtones when in silent mode

--- SessionRinger.py
+++ SessionRinger.py
@@ -45,19 +45,21 @@ class Ringer(object):
     filesend_sessions = {}
     filerecv_sessions = {}
     active_sessions = set()
-    inbound_ringtone = None
-    tone_ringtone = None
-    initial_hold_tone = None
-    hold_tone = None
-    chat_ringtone = None
-    tone_chat_ringtone = None
     on_hold_session_count = 0
 
-    msg_out_sound = None
-    msg_in_sound = None
+    audio_primary_ringtone = None
+    audio_secondary_ringtone = None
+    initial_hold_tone = None
+    secondary_hold_tone = None
 
-    file_out_sound = None
-    file_in_sound = None
+    chat_primary_ringtone = None
+    chat_secondary_ringtone = None
+
+    chat_message_outgoing_sound = None
+    chat_message_incoming_sound = None
+
+    file_transfer_outgoing_sound = None
+    file_transfer_incoming_sound = None
 
     last_hangup_tone_time = 0
     chat_beep_time = 0
@@ -89,23 +91,26 @@ class Ringer(object):
         notification_center.remove_observer(self, name="BlinkWillCancelProposal")
 
     def update_playing_ringtones(self, account=None):
-        should_play_incoming = False
-        should_play_incoming_tone = False
-        should_play_chat = False
-        should_play_chat_tone = False
+        settings = SIPSimpleSettings()
+
+        should_play_audio_primary_ringtone = False
+        should_play_audio_primary_ringtone_tone = False
+        should_play_chat_primary_ringtone = False
+        should_play_chat_secondary_ringtone = False
 
         if 'audio' in (stream.type for stream in chain(*(session.streams for session in self.active_sessions if session.streams))):
             # play only secondary ringtones when there's active audio sessions
+
             if self.incoming_audio_sessions:
-                should_play_incoming_tone = True
+                should_play_audio_primary_ringtone_tone = True
             if self.chat_sessions or self.filerecv_sessions or self.ds_sessions:
-                should_play_chat_tone = True
+                should_play_chat_secondary_ringtone = True
         else:
             # play ringtone
             if self.incoming_audio_sessions:
-                should_play_incoming = True
+                should_play_audio_primary_ringtone = True
             if self.chat_sessions or self.filerecv_sessions or self.ds_sessions:
-                should_play_chat = True
+                should_play_chat_primary_ringtone = True
 
         if self.ringing_sessions:
             # proposal for adding new streams to an ongoing session
@@ -114,13 +119,12 @@ class Ringer(object):
             has_ds_proposed = 'desktop-sharing' in proposed_stream_types
             has_file_proposed = 'file-transfer' in proposed_stream_types
             has_audio_ongoing = 'audio' in (stream.type for stream in chain(*(session.streams for session in self.ringing_sessions if session.streams)))
-            should_play_chat_tone = (has_chat_proposed or has_file_proposed or has_ds_proposed) and not has_audio_ongoing
+            should_play_chat_secondary_ringtone = (has_chat_proposed or has_file_proposed or has_ds_proposed) and not has_audio_ongoing
 
-        if self.inbound_ringtone:
-            if should_play_incoming and not self.inbound_ringtone.is_active:
+        if self.audio_primary_ringtone:
+            if should_play_audio_primary_ringtone and not self.audio_primary_ringtone.is_active:
                 self.update_ringtones(account)
 
-                settings = SIPSimpleSettings()
                 this_hour = int(datetime.datetime.now(tzlocal()).strftime("%H"))
                 volume = None
                 if settings.sounds.night_volume.start_hour < settings.sounds.night_volume.end_hour:
@@ -141,74 +145,75 @@ class Ringer(object):
                     else:
                         volume = settings.sounds.audio_inbound.volume
 
-                self.inbound_ringtone.volume = volume
-                self.inbound_ringtone.start()
-            elif not should_play_incoming and self.inbound_ringtone.is_active:
-                self.inbound_ringtone.stop()
+                self.audio_primary_ringtone.volume = volume
+                self.audio_primary_ringtone.start()
+            elif not should_play_audio_primary_ringtone and self.audio_primary_ringtone.is_active:
+                self.audio_primary_ringtone.stop()
 
-        if self.tone_ringtone:
-            if should_play_incoming_tone and not self.tone_ringtone.is_active:
-                self.tone_ringtone.start()
-            elif not should_play_incoming_tone and self.tone_ringtone.is_active:
-                self.tone_ringtone.stop()
+        if self.audio_secondary_ringtone:
+            if should_play_audio_primary_ringtone_tone and not self.audio_secondary_ringtone.is_active:
+                self.audio_secondary_ringtone.start()
+            elif not should_play_audio_primary_ringtone_tone and self.audio_secondary_ringtone.is_active:
+                self.audio_secondary_ringtone.stop()
 
-        if self.chat_ringtone:
-            if should_play_chat and not self.chat_ringtone.is_active:
-                self.chat_ringtone.start()
-            elif not should_play_chat and self.chat_ringtone.is_active:
-                self.chat_ringtone.stop()
+        if self.chat_primary_ringtone:
+            if should_play_chat_primary_ringtone and not self.chat_primary_ringtone.is_active:
+                self.chat_primary_ringtone.start()
+            elif not should_play_chat_primary_ringtone and self.chat_primary_ringtone.is_active:
+                self.chat_primary_ringtone.stop()
 
-        if self.tone_chat_ringtone:
-            if should_play_chat_tone and not self.tone_chat_ringtone.is_active:
-                self.tone_chat_ringtone.start()
-            elif not should_play_chat_tone and self.tone_chat_ringtone.is_active:
-                self.tone_chat_ringtone.stop()
+        if self.chat_secondary_ringtone:
+            if should_play_chat_secondary_ringtone and not self.chat_secondary_ringtone.is_active:
+                self.chat_secondary_ringtone.start()
+            elif not should_play_chat_secondary_ringtone and self.chat_secondary_ringtone.is_active:
+                self.chat_secondary_ringtone.stop()
 
     def update_ringtones(self, account=None):
+        settings = SIPSimpleSettings()
+
         if account is None:
             account = AccountManager().default_account
-        settings = SIPSimpleSettings()
+
         app = SIPApplication()
 
         def change_tone(name, new_tone):
             current = getattr(self, name)
-            if current:
-                if current.is_active:
-                    current.stop()
-                    if new_tone:
-                        new_tone.start()
+            if current and current.is_active:
+                current.stop()
+                if new_tone:
+                    new_tone.start()
             setattr(self, name, new_tone)
 
         change_tone("initial_hold_tone", WavePlayer(app.voice_audio_mixer, Resources.get('hold_tone.wav'), volume=10))
         app.voice_audio_bridge.add(self.initial_hold_tone)
-        change_tone("hold_tone", WavePlayer(app.voice_audio_mixer, Resources.get('hold_tone.wav'), loop_count=0, pause_time=45, volume=10, initial_play=False))
-        app.voice_audio_bridge.add(self.hold_tone)
+        change_tone("secondary_hold_tone", WavePlayer(app.voice_audio_mixer, Resources.get('hold_tone.wav'), loop_count=0, pause_time=45, volume=10, initial_play=False))
+        app.voice_audio_bridge.add(self.secondary_hold_tone)
 
         if account:
-            inbound_ringtone = account.sounds.audio_inbound.sound_file if account.sounds.audio_inbound is not None else None
+            audio_primary_ringtone = account.sounds.audio_inbound.sound_file if account.sounds.audio_inbound is not None else None
         else:
-            inbound_ringtone = None
+            audio_primary_ringtone = None
 
-        if inbound_ringtone and not settings.audio.silent:
+        if audio_primary_ringtone and not settings.audio.silent:
             # Workaround not to use same device from two bridges. -Saul
             if settings.audio.alert_device is not None and app.alert_audio_mixer.real_output_device == app.voice_audio_mixer.real_output_device:
-                new_tone = WavePlayer(app.voice_audio_mixer, inbound_ringtone.path, loop_count=0, pause_time=6)
+                new_tone = WavePlayer(app.voice_audio_mixer, audio_primary_ringtone.path, loop_count=0, pause_time=6)
                 app.voice_audio_bridge.add(new_tone)
             else:
-                new_tone = WavePlayer(app.alert_audio_mixer, inbound_ringtone.path, loop_count=0, pause_time=6)
+                new_tone = WavePlayer(app.alert_audio_mixer, audio_primary_ringtone.path, loop_count=0, pause_time=6)
                 app.alert_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("inbound_ringtone", new_tone)
+        change_tone("audio_primary_ringtone", new_tone)
 
-        if inbound_ringtone and not settings.audio.silent:
+        if audio_primary_ringtone and not settings.audio.silent:
             new_tone = WavePlayer(app.voice_audio_mixer, Resources.get('ring_tone.wav'), loop_count=0, pause_time=6)
             app.voice_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("tone_ringtone", new_tone)
+        change_tone("audio_secondary_ringtone", new_tone)
 
-        if inbound_ringtone and not settings.audio.silent:
+        if audio_primary_ringtone and not settings.audio.silent:
             # Workaround not to use same device from two bridges. -Saul
             if settings.audio.alert_device is not None and app.alert_audio_mixer.real_output_device == app.voice_audio_mixer.real_output_device:
                 new_tone = WavePlayer(app.voice_audio_mixer, Resources.get('ring_tone.wav'), loop_count=0, pause_time=6)
@@ -218,50 +223,51 @@ class Ringer(object):
                 app.alert_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("chat_ringtone", new_tone)
+        change_tone("chat_primary_ringtone", new_tone)
 
-        if inbound_ringtone and not settings.audio.silent:
+        if audio_primary_ringtone and not settings.audio.silent:
             new_tone = WavePlayer(app.voice_audio_mixer, Resources.get('ring_tone.wav'), loop_count=0, pause_time=6)
             app.voice_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("tone_chat_ringtone", new_tone)
+        change_tone("chat_secondary_ringtone", new_tone)
 
-        msg_out_sound = settings.sounds.message_sent
-        if msg_out_sound and not settings.audio.silent:
-            new_tone = WavePlayer(app.voice_audio_mixer, msg_out_sound.path, volume=msg_out_sound.volume)
+        chat_message_outgoing_sound = settings.sounds.message_sent
+        if chat_message_outgoing_sound and not settings.audio.silent:
+            new_tone = WavePlayer(app.voice_audio_mixer, chat_message_outgoing_sound.path, volume=chat_message_outgoing_sound.volume)
             app.voice_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("msg_out_sound", new_tone)
+        change_tone("chat_message_outgoing_sound", new_tone)
 
-        msg_in_sound = settings.sounds.message_received
-        if msg_in_sound and not settings.audio.silent:
-            new_tone = WavePlayer(app.voice_audio_mixer, msg_in_sound.path, volume=msg_in_sound.volume)
+        chat_message_incoming_sound = settings.sounds.message_received
+        if chat_message_incoming_sound and not settings.audio.silent:
+            new_tone = WavePlayer(app.voice_audio_mixer, chat_message_incoming_sound.path, volume=chat_message_incoming_sound.volume)
             app.voice_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("msg_in_sound", new_tone)
+        change_tone("chat_message_incoming_sound", new_tone)
 
-        file_out_sound = settings.sounds.file_sent
-        if file_out_sound and not settings.audio.silent:
-            new_tone = WavePlayer(app.voice_audio_mixer, file_out_sound.path, volume=file_out_sound.volume)
+        file_transfer_outgoing_sound = settings.sounds.file_sent
+        if file_transfer_outgoing_sound and not settings.audio.silent:
+            new_tone = WavePlayer(app.voice_audio_mixer, file_transfer_outgoing_sound.path, volume=file_transfer_outgoing_sound.volume)
             app.voice_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("file_out_sound", new_tone)
+        change_tone("file_transfer_outgoing_sound", new_tone)
 
-        file_in_sound = settings.sounds.file_received
-        if file_in_sound and not settings.audio.silent:
-            new_tone = WavePlayer(app.voice_audio_mixer, file_in_sound.path, volume=file_in_sound.volume)
+        file_transfer_incoming_sound = settings.sounds.file_received
+        if file_transfer_incoming_sound and not settings.audio.silent:
+            new_tone = WavePlayer(app.voice_audio_mixer, file_transfer_incoming_sound.path, volume=file_transfer_incoming_sound.volume)
             app.voice_audio_bridge.add(new_tone)
         else:
             new_tone = None
-        change_tone("file_in_sound", new_tone)
+        change_tone("file_transfer_incoming_sound", new_tone)
 
     def add_incoming(self, session, streams):
+        settings = SIPSimpleSettings()
         stream_types = [s.type for s in streams]
-        if 'audio' in stream_types:
+        if 'audio' in stream_types and not settings.audio.silent:
             self.incoming_audio_sessions[session] = streams
         else:
             if 'chat' in stream_types:
@@ -274,7 +280,6 @@ class Ringer(object):
     def add_outgoing(self, session, streams):
         NotificationCenter().add_observer(self, sender=session)
 
-
     def stop_ringing(self, session):
         self.ringing_sessions.discard(session)
         self.incoming_audio_sessions.pop(session, None)
@@ -284,6 +289,11 @@ class Ringer(object):
         self.update_playing_ringtones()
 
     def play_hangup(self):
+        settings = SIPSimpleSettings()
+
+        if settings.audio.silent:
+            return
+
         if time.time() - self.last_hangup_tone_time > HANGUP_TONE_THROTLE_DELAY:
             hangup_tone = WavePlayer(SIPApplication.voice_audio_mixer, Resources.get('hangup_tone.wav'), volume=30)
             NotificationCenter().add_observer(self, sender=hangup_tone, name="WavePlayerDidEnd")
@@ -301,13 +311,6 @@ class Ringer(object):
         if name == "SIPSessionWillStart":
             self.active_sessions.add(session)
             self.stop_ringing(session)
-            #if session.direction == "incoming":
-            #    if self.inbound_ringtone:
-            #        self.inbound_ringtone.stop()
-            #    if len(self.incoming_audio_sessions) <= 1 and self.tone_ringtone:
-            #        self.tone_ringtone.stop()
-        elif name == "SIPSessionDidStart":
-            pass
         elif name in ("SIPSessionWillEnd", "SIPSessionDidFail"):
             streams = session.streams or session.proposed_streams
             if not streams:
@@ -346,38 +349,40 @@ class Ringer(object):
             self.ringing_sessions.add(session)
             self.update_playing_ringtones()
         elif name == "AudioStreamDidChangeHoldState":
-            if data.on_hold:
-                self.on_hold_session_count += 1
-            else:
-                self.on_hold_session_count -= 1
-            if self.hold_tone:
-                if self.on_hold_session_count == 1:
-                    self.hold_tone.start()
-                elif self.on_hold_session_count == 0:
-                    self.hold_tone.stop()
-            if data.on_hold and data.originator == 'remote' and self.initial_hold_tone and not self.initial_hold_tone.is_active:
-                self.initial_hold_tone.start()
+            if not settings.audio.silent:
+                if data.on_hold:
+                    self.on_hold_session_count += 1
+                else:
+                    self.on_hold_session_count -= 1
+                if self.secondary_hold_tone:
+                    if self.on_hold_session_count == 1:
+                        self.secondary_hold_tone.start()
+                    elif self.on_hold_session_count == 0:
+                        self.secondary_hold_tone.stop()
+                if data.on_hold and data.originator == 'remote' and self.initial_hold_tone and not self.initial_hold_tone.is_active:
+                    self.initial_hold_tone.start()
         elif name == "ConferenceHasAddedAudio":
             # TODO: play-resume iTunes after adding audio to conference tone has been played -adi
             self.initial_hold_tone.start()
         elif name == "ChatViewControllerDidDisplayMessage":
-            now = time.time()
-            if not settings.audio.silent and self.msg_out_sound and now - self.chat_beep_time > CHAT_TONE_THROTLE_DELAY and not data.history_entry:
-                if data.direction == 'outgoing':
-                    self.msg_out_sound.stop()
-                    self.msg_out_sound.start()
-                else:
-                    self.msg_in_sound.stop()
-                    self.msg_in_sound.start()
-                self.chat_beep_time = now
+            if not settings.audio.silent:
+                now = time.time()
+                if self.chat_message_outgoing_sound and now - self.chat_beep_time > CHAT_TONE_THROTLE_DELAY and not data.history_entry:
+                    if data.direction == 'outgoing':
+                        self.chat_message_outgoing_sound.stop()
+                        self.chat_message_outgoing_sound.start()
+                    else:
+                        self.chat_message_incoming_sound.stop()
+                        self.chat_message_incoming_sound.start()
+                    self.chat_beep_time = now
         elif name == "BlinkFileTransferDidEnd":
             if not settings.audio.silent:
                 if data == "send":
-                    if self.file_out_sound:
-                        self.file_out_sound.start()
+                    if self.file_transfer_outgoing_sound:
+                        self.file_transfer_outgoing_sound.start()
                 else:
-                    if self.file_in_sound:
-                        self.file_in_sound.start()
+                    if self.file_transfer_incoming_sound:
+                        self.file_transfer_incoming_sound.start()
         elif name == "CFGSettingsObjectDidChange":
             sound_attributes = ['audio.silent', 'audio.alert_device', 'audio.output_device', 'sounds.audio_inbound', 'sounds.message_sent', 'sounds.message_received', 'sounds.file_sent', 'sounds.file_received']
             if set(sound_attributes).intersection(data.modified):
@@ -386,8 +391,9 @@ class Ringer(object):
                 self.update_playing_ringtones()
         elif name == "WavePlayerDidEnd":
             NotificationCenter().remove_observer(self, sender=notification.sender, name="WavePlayerDidEnd")
+
         on_hold_streams = [stream for stream in chain(*(session.streams for session in self.active_sessions if session.streams)) if stream.on_hold]
-        if not on_hold_streams and self.hold_tone is not None and self.hold_tone.is_active:
-            self.hold_tone.stop()
+        if not on_hold_streams and self.secondary_hold_tone is not None and self.secondary_hold_tone.is_active:
+            self.secondary_hold_tone.stop()
 
 
