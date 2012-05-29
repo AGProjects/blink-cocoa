edb5cbabba0b00de0f13f953597c1c7d333cf5d6
Adrian Georgescu <ag@ag-projects.com>
2012-05-29 10:59:09 +0000

    Changed default registration period back to 600 seconds

--- configuration/account.py
+++ configuration/account.py
@@ -73,7 +73,7 @@ class SIPSettingsExtension(SIPSettings):
     always_use_my_proxy = Setting(type=bool, default=True)
     register = Setting(type=bool, default=True)
     do_not_disturb_code = Setting(type=NonNegativeInteger, default=486, nillable=False)
-    register_interval = Setting(type=NonNegativeInteger, default=3600)
+    register_interval = Setting(type=NonNegativeInteger, default=600)
 
 
 class ServerSettings(SettingsGroup):
