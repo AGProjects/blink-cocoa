7097e7597c11121743c23f49d2468bc2a12568cf
Adrian Georgescu <ag@ag-projects.com>
2013-01-08 13:21:10 +0000

    Print selected ICE candidates

--- AudioController.py
+++ AudioController.py
@@ -1027,6 +1027,9 @@ class AudioController(MediaStream):
             self.end()
 
     def _NH_AudioStreamICENegotiationDidSucceed(self, sender, data):
+        self.sessionController.log_info(u'ICE negotiation suceeded')
+        self.sessionController.log_info(u'Audio RTP endpoints: %s:%d (%s) <-> %s:%d (%s)' % (self.stream.local_rtp_address, self.stream.local_rtp_port, self.stream.local_rtp_candidate_type, self.stream.remote_rtp_address, self.stream.remote_rtp_port, self.stream.remote_rtp_candidate_type))
+
         self.ice_negotiation_status = 'Success'
 
     def _NH_BlinkAudioStreamUnholdRequested(self, sender, data):
