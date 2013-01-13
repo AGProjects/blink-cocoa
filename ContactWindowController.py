14c3275136f3ad4adbe169aa42e4f6d00d6b568c
Adrian Georgescu <ag@ag-projects.com>
2013-01-13 14:33:51 +0000

    Renamed menu item

--- ContactWindowController.py
+++ ContactWindowController.py
@@ -3710,7 +3710,7 @@ class ContactWindowController(NSWindowController):
                 mitem.setEnabled_(True)
                 mitem.setRepresentedObject_(item)
 
-                mitem = self.contactContextMenu.addItemWithTitle_action_keyEquivalent_("Hide My Availability to %s" % item.name , "setPresencePolicy:", "")
+                mitem = self.contactContextMenu.addItemWithTitle_action_keyEquivalent_("Block my Availability to %s" % item.name , "setPresencePolicy:", "")
                 mitem.setState_(NSOnState if item.contact.presence.policy == 'block' else NSOffState)
                 mitem.setEnabled_(True)
                 mitem.setRepresentedObject_(item)
