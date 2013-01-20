3b7072b02753f49c90655eec2293f3b2986a173f
Adrian Georgescu <ag@ag-projects.com>
2013-01-20 14:53:54 +0000

    Lower sip refresh intervals

--- configuration/account.py
+++ configuration/account.py
@@ -87,7 +87,9 @@ class SIPSettingsExtension(SIPSettings):
     always_use_my_proxy = Setting(type=bool, default=True)
     register = Setting(type=bool, default=True)
     do_not_disturb_code = Setting(type=NonNegativeInteger, default=486, nillable=False)
-    register_interval = Setting(type=NonNegativeInteger, default=600)
+    register_interval = Setting(type=NonNegativeInteger, default=300)
+    subscribe_interval = Setting(type=NonNegativeInteger, default=300)
+    publish_interval = Setting(type=NonNegativeInteger, default=300)
 
 
 class ServerSettings(SettingsGroup):
