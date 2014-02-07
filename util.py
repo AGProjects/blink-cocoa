02810e038549256864248f27f6543d70018b10d7
Adrian Georgescu <ag@ag-projects.com>
2014-02-07 16:18:39 +0000

    Improve anonymous user detection

--- util.py
+++ util.py
@@ -242,7 +242,12 @@ def is_anonymous(uri):
     """
         Check if the given URI is an anonymous uri
         """
+    anon_users = ('asterisk', 'unknown', 'anonymous')
+
     if isinstance(uri, (SIPURI, FrozenSIPURI)):
+        if uri.user.lower() in anon_users:
+            return True
+
         return (uri.user is None or uri.user.lower() == 'anonymous') and (uri.host is None or uri.host.lower() == 'anonymous.invalid')
     else:
         if not (uri.startswith('sip:') or uri.startswith('sips:')):
@@ -252,6 +257,9 @@ def is_anonymous(uri):
         except:
             return False
         else:
+            if sip_uri.user is not None and sip_uri.user.lower() in anon_users:
+                return True
+
             return (sip_uri.user is None or sip_uri.user.lower() == 'anonymous') and (sip_uri.host is None or sip_uri.host.lower() == 'anonymous.invalid')
 
 def format_size(s, minsize=0, bits=False):
