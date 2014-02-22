8d813e1a2c0bc4a0c85697396784f6fd7aeabf51
Adrian Georgescu <ag@ag-projects.com>
2014-02-22 19:32:10 +0000

    Xib translation changes

--- en.lproj/generate_translatable_strings_in_xibs.sh
+++ en.lproj/generate_translatable_strings_in_xibs.sh
@@ -2,10 +2,12 @@
 # http://www.aboveground.com/blog/xcode-4-4-and-localizable-strings-no-way-to-add-language
 # http://www.cocoawithlove.com/2011/04/user-interface-strings-in-cocoa.html
 
+#http://kb.applingua.com/2011/10/extracting-and-localizing-xibs-ibtool/
+
 for file in *.xib; do
     ibtool --export-strings-file "$file".strings "$file"
-    for d in ../*.lproj; do
-        cp -r *.xib.strings $d/
-    done
-    rm *.xib.strings
+#    for d in ../*.lproj; do
+#        cp -r *.xib.strings $d/
+#    done
+#    rm *.xib.strings
 done
