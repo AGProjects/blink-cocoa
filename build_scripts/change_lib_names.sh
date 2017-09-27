fa774260a8afa1225694a19b40db2a0e762bb5c4
Adrian Georgescu <ag@ag-projects.com>
2017-09-27 18:35:28 +0000

    Add more paths to change lib names script

--- build_scripts/change_lib_names.sh
+++ build_scripts/change_lib_names.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-old_path="local/lib/\|local/Cellar/\|/usr/local/opt/libmpc/lib/\|/usr/local/opt/mpfr/lib/\|Frameworks/Frameworks/\|/Users/adigeo/work/ag-projects/video/local/lib/"
+old_path="/usr/local/opt/python/Frameworks/\|/usr/local/opt/openssl/lib/\|local/opt/\|local/lib/\|local/Cellar/\|/usr/local/opt/libmpc/lib/\|/usr/local/opt/mpfr/lib/\|Frameworks/Frameworks/\|/Users/adigeo/work/ag-projects/video/local/lib/"
 new_path="@executable_path/../Frameworks/"
 
 for library in $@; do
