33f625f41f7a132391d3282f7d8d7e677648a736
Adrian Georgescu <ag@ag-projects.com>
2021-02-13 11:50:20 +0000

    Improve gateway check script

--- build_scripts/gatekeeper.sh
+++ build_scripts/gatekeeper.sh
@@ -1,9 +1,12 @@
 echo "Blink"
-spctl --verbose=1 --assess staging/Blink.app
-codesign --verify --verbose=1 staging/Blink.app/
 
-echo
+#https://developer.apple.com/library/archive/technotes/tn2206/_index.html#//apple_ref/doc/uid/DTS40007919-CH1-TNTAG211
+codesign --verify --deep --strict --verbose=3 Frameworks/Python.framework/
+codesign --verify --deep --strict --verbose=3 Frameworks/Sparkle.framework
+codesign --verify --deep --strict --verbose=3 Frameworks/*.dylib
+codesign --verify --deep --strict --verbose=3 staging/Blink.app/
 
-echo "SIP2SIP"
-spctl --verbose=1 --assess staging_sip2sip/SIP2SIP.app
-codesign --verify --verbose=1 staging_sip2sip/SIP2SIP.app/
+echo "Gatekeeper"
+#spctl --verbose=1 --assess staging/Blink.app
+#codesign --verify --verbose=1 staging/Blink.app/
+spctl -a -t exec -vvv staging/Blink.app
