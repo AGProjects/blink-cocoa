87b4f5d07038ca7da3885950bf4092bf5fc1f170
Adrian Georgescu <ag@ag-projects.com>
2021-02-15 22:26:17 +0000

    Increase year

--- BlinkAppDelegate.py
+++ BlinkAppDelegate.py
@@ -144,7 +144,7 @@ class BlinkAppDelegate(NSObject):
                            }
 
     statusbar_menu_icon = 'invisible'
-    about_copyright = "Copyright 2009-2014 AG Projects"
+    about_copyright = "Copyright 2009-2021 AG Projects"
     active_transports = set()
     terminating = False
 
