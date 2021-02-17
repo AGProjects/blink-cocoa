51a7913f3ddfe8f0b8aa9b4b1f1b456cf11dde92
Adrian Georgescu <ag@ag-projects.com>
2021-02-17 14:02:44 +0000

    Update code sign script

--- build_scripts/codesign.sh
+++ build_scripts/codesign.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-id="Developer ID Application"
-sos=`find ./Resources/lib -name *.so`; for s in $sos; do codesign -f -s "$id" $s; done
-sos=`find ./Frameworks -name *.dylib`; for s in $sos; do codesign -f -s "$id" $s; done
-sos=`find ./Frameworks -name *.so`; for s in $sos; do codesign -f -s "$id" $s; done
-sos=`find ./Frameworks -name *.o`; for s in $sos; do codesign -f -s "$id" $s; done
-sos=`find ./Frameworks -name *.a`; for s in $sos; do codesign -f -s "$id" $s; done
+sos=`find ./Resources -name *.dylib`; for s in $sos; do codesign -f -o runtime --timestamp --entitlements Entitlements.plist -s "Developer ID Application" $s; done
+sos=`find ./Resources -name *.so`; for s in $sos; do codesign -f -o runtime --timestamp --entitlements Entitlements.plist -s "Developer ID Application" $s; done
+sos=`find ./Frameworks -name *.dylib`; for s in $sos; do codesign -f -o runtime --timestamp --entitlements Entitlements.plist -s "Developer ID Application" $s; done
+sos=`find ./Frameworks -name *.so`; for s in $sos; do codesign -f -o runtime --timestamp --entitlements Entitlements.plist -s "Developer ID Application" $s; done
+codesign -f -o runtime --timestamp --entitlements Entitlements.plist -s "Developer ID Application" Frameworks/Python.framework
