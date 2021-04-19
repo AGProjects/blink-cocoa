c9a3f004899d472fd719ffea5288fe2494dbc774
Adrian Georgescu <ag@ag-projects.com>
2021-04-19 12:56:08 +0000

    Update build scripts

--- build_scripts/copy_sipsimple.sh
+++ build_scripts/copy_sipsimple.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cp -a ~/Library/Python/3.9/lib/python/site-packages/sipsimple ../Distribution/Resources/lib/
+cp -a ~/work/python3-sipsimple/sipsimple ../Distribution/Resources/lib/
 
-./build_scripts/change_lib_names.sh ../Distribution/Resources/lib/sipsimple/core/_core.cpython-39-darwin.so
+../build_scripts/change_lib_names.sh ../Distribution/Resources/lib/sipsimple/core/_core.cpython-39-darwin.so
+../build_scripts/change_lib_names.sh ../Distribution/Resources/lib/sipsimple/util/_sha1.cpython-39-darwin.so
 
-sign_id="Developer ID Application"
-codesign -f -s "$sign_id" ../Distribution/Resources/lib/sipsimple/core/_core.cpython-39-darwin.so
+codesign -f -s "Developer ID Application" ../Distribution/Resources/lib/sipsimple/core/_core.cpython-39-darwin.so
+codesign -f -s "Developer ID Application" ../Distribution/Resources/lib/sipsimple/util/_sha1.cpython-39-darwin.so
