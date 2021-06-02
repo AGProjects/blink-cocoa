183132dd88357448fe2388fa2416fcc9c819d4c0
Adrian Georgescu <ag@ag-projects.com>
2021-06-02 12:38:00 +0000

    Added AMR wideband codec

--- configuration/settings.py
+++ configuration/settings.py
@@ -119,7 +119,7 @@ class GUISettings(SettingsGroup):
     close_delay = Setting(type=NonNegativeInteger, default=4)
 
 class RTPSettingsExtension(RTPSettings):
-    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA', 'AMR')))
+    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA', 'AMR-WB', 'AMR')))
     video_codec_list = Setting(type=VideoCodecList, default=VideoCodecList(('H264', 'VP8', 'VP9')))
 
 class ServiceProviderSettings(SettingsGroup):
