802033969d6b78eb0a0132789591e42bb3ae6f19
Adrian Georgescu <ag@ag-projects.com>
2021-06-06 16:14:22 +0000

    Update audio codecs

--- configuration/settings.py
+++ configuration/settings.py
@@ -119,7 +119,7 @@ class GUISettings(SettingsGroup):
     close_delay = Setting(type=NonNegativeInteger, default=4)
 
 class RTPSettingsExtension(RTPSettings):
-    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'G722', 'PCMU', 'PCMA', 'AMR-WB', 'AMR')))
+    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'speex', 'G722', 'GSM', 'iLBC', 'PCMU', 'PCMA', 'AMR-WB', 'AMR-NB')))
     video_codec_list = Setting(type=VideoCodecList, default=VideoCodecList(('H264', 'VP8', 'VP9')))
 
 class ServiceProviderSettings(SettingsGroup):
