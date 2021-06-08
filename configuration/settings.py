f284d2bdccb2779797f4c4eccb1cabc858d6212d
Adrian Georgescu <ag@ag-projects.com>
2021-06-08 21:42:18 +0000

    Remove AMR-NB codec

--- configuration/settings.py
+++ configuration/settings.py
@@ -118,9 +118,11 @@ class GUISettings(SettingsGroup):
     media_support_detection = Setting(type=bool, default=False)
     close_delay = Setting(type=NonNegativeInteger, default=4)
 
+
 class RTPSettingsExtension(RTPSettings):
-    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'speex', 'G722', 'GSM', 'iLBC', 'PCMU', 'PCMA', 'AMR-WB', 'AMR-NB')))
+    audio_codec_list = Setting(type=AudioCodecList, default=AudioCodecList(('opus', 'speex', 'G722', 'GSM', 'iLBC', 'PCMU', 'PCMA', 'AMR-WB')))
     video_codec_list = Setting(type=VideoCodecList, default=VideoCodecList(('H264', 'VP8', 'VP9')))
+
 
 class ServiceProviderSettings(SettingsGroup):
     name = Setting(type=str, default=None, nillable=True)
