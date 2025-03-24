7d0d7ed715030d75d198e13df57303e4ae6726fa
Adrian Georgescu <ag@ag-projects.com>
2025-03-24 21:42:05 +0000

    Fix dmg

--- build_scripts/publish/06-upload.sh
+++ build_scripts/publish/06-upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rsync -avz --progress ../../Distribution/Blink.dmg cdr-adm:/var/www/prj/dnshosting/blink/Releases/MacOSX/
+rsync -avz --progress dmg/Blink.dmg cdr-adm:/var/www/prj/dnshosting/blink/Releases/MacOSX/
 rsync -avz ../../ReleaseNotes/BlinkAppcast.xml cdr-adm:/var/www/prj/dnshosting/blink/
 rsync -avz ../../ReleaseNotes/changelog-beta.html cdr-adm:/var/www/prj/dnshosting/blink/ReleaseNotes/
