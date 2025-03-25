4e0b8bda0ff3aabb769e0e43769d0573e631fe99
Adrian Georgescu <ag@ag-projects.com>
2025-03-25 12:46:34 +0000

    Update build scripts

--- build_scripts/05-copy-libraries.sh
+++ build_scripts/05-copy-libraries.sh
@@ -21,39 +21,59 @@ fi
 echo $core
 
 libs=`./get_deps_recurrent.py $core`
-mkdir Frameworks/libs
+
 lib_dir="Frameworks/libs"
+mkdir $lib_dir
+mkdir $lib_dir-x86_64
+mkdir $lib_dir-arm64
 
 for l in $libs; do
         fn=`basename $l`
-        echo "Checking SDK dependency $l"
-#        if [ ! -f $lib_dir/$fn ]; then
-            echo "cp $l to $lib_dir/"
-            cp $l $lib_dir/
-            ../build_scripts/change_lib_paths.sh $lib_dir/$fn
-            codesign -f --timestamp -s "Developer ID Application" $lib_dir/$fn
- #       else
- #           file $lib_dir/$fn
- #       fi
+        #echo "Checking SDK dependency $l"
+            #echo "cp $l to $lib_dir/"
+            #cp $l $lib_dir/
+            ARCH=$(lipo -info $l | awk -F ': ' '{print $3}')
+            ARCH=${ARCH// /}
+            if [[ "$ARCH" == "x86_64arm64" ]]; then
+                dst=$lib_dir/
+            else
+                dst=$lib_dir-$ARCH/
+            fi
+         
+       dst=$lib_dir/
+         
+       if [ ! -f $dst/$fn ]; then   
+            echo "cp $l $dst"
+            cp $l $dst
+            ../build_scripts/change_lib_paths.sh $dst/$fn
+            codesign -f --timestamp -s "Developer ID Application" $dst/$fn
+       else
+            lipo -info $dst/$fn
+        fi
 done
 
 lib_dir="Frameworks/libs"
-extra_libs="/opt/local/lib/libmpfr.6.dylib /opt/local/lib/libmpc.3.dylib /opt/local/lib/libuuid.1.dylib"
+extra_libs="/opt/local/lib/libmpfr.6.dylib /opt/local/lib/libmpc.3.dylib /opt/local/lib/libuuid.1.dylib /opt/local/lib/libgnutls.30.dylib"
+gnutls_libs=`./get_deps_recurrent.py /opt/local/lib/libgnutls.30.dylib`
 
-for l in $extra_libs; do
+
+
+for l in $extra_libs $gnutls_libs; do
     fn=`basename $l`
     echo $lib_dir/$fn
-    if [ ! -f $lib_dir/$fn ]; then
+#    if [ ! -f $lib_dir/$fn ]; then
         echo "Copy library $l to $lib_dir/"
         cp $l $lib_dir/
         ../build_scripts/change_lib_paths.sh $lib_dir/$fn
         codesign -f --timestamp -s "Developer ID Application" $lib_dir/$fn
-    fi
+#    fi
 done
 
 if [ ! -d Frameworks/Python.framework/Versions ]; then
     exit 0
 fi
+
+lipo -info Frameworks/Python.framework/Versions/$pver/lib/libpython$pver.dylib
 
 cp Frameworks/Python.framework/Versions/$pver/lib/libpython$pver.dylib Frameworks/libs/
 cd Frameworks/libs/
